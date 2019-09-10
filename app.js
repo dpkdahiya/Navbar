bbad8323ba4c23d65397302f93b7140386550f3f
dpkdahiya <39999569+dpkdahiya@users.noreply.github.com>
2019-09-10 22:52:48 +0530

    Navbar

---
+++ app.js
@@ -0,0 +1,24 @@
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click',() => {
+        //toggle
+        nav.classList.toggle('nav-active');
+        
+        //anim
+    navLinks.forEach((link,index) => {
+        if(link.style.animation){
+            link.style.animation='';
+        } else {
+            link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 +1.5}s';
+        }
+            
+    });
+            //bg anim
+            burger.classList.toggle('toggle');
+});
+
+}
+
+navslide();
